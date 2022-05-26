33203d06f0244f168cbeedf93d3a9a6ec6d0a042
Tomas Zazzali <tommyzazzali@gmail.com>
2022-05-26 17:32:51 +0200

    Firebase: Commit previo a conectar

---
+++ reactchnuevo/src/firebase/config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAfxu3P8chukrZPwT0_eI7kzaCDO47-oS8",
+
+  authDomain: "reactecommercezazzali.firebaseapp.com",
+
+  projectId: "reactecommercezazzali",
+
+  storageBucket: "reactecommercezazzali.appspot.com",
+
+  messagingSenderId: "519289380086",
+
+  appId: "1:519289380086:web:ccfb4d5c6b76e93c87e0d7"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
--- reactchnuevo/src/index.js
+++ reactchnuevo/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAfxu3P8chukrZPwT0_eI7kzaCDO47-oS8",
+  authDomain: "reactecommercezazzali.firebaseapp.com",
+  projectId: "reactecommercezazzali",
+  storageBucket: "reactecommercezazzali.appspot.com",
+  messagingSenderId: "519289380086",
+  appId: "1:519289380086:web:ccfb4d5c6b76e93c87e0d7"
+};
+
+// Initialize Firebase
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
